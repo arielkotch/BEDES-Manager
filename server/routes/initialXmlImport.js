439d3a88e36fbba6c67e4d911dcc759345a61b1b
Ted Shin <tdyshin@gmail.com>
2017-10-29 22:31:39 -0400

    Initial xml import into DB only runs if Term collection has no documents

--- server/routes/initialXmlImport.js
+++ server/routes/initialXmlImport.js
@@ -7,105 +7,114 @@ const Term = require('../models/term');
 
 const initalXmlImport = (app) => {
   app.get('/initial-import', function(req, res) {
-    // Parser for converting xml to json
-    const parser = new xml2js.Parser();
-    // Parse terms xml and convert to json
-    fs.readFile(path.resolve(__dirname, '../../resources/bedes_online_dictionary_uuid-terms.xml'), function(err, data) {
-      parser.parseString(data, function (err, result) {
-        let termsList = result.nodes.node;
-        // Save terms to DB
-        termsList.forEach((term, index) => {
-          let newTerm = new Term();
-          // Set the term's keys and values
-          newTerm['Content-UUID'] = term['Content-UUID'][0];
-          newTerm['URL'] = term['URL'][0];
-          newTerm['Term'] = term['Term'][0];
-          newTerm['Updated-date'] = term['Updated-date'][0];
-          newTerm['Category'] = term['Category'][0];
-          // Check if Term-Definition exist in current term
-          if (term['Term-Definition'][0]['p']) {
-            // This is the simplest case when Term-Definition does not have an <a> or <span> nested inside of it.
-            // Most of the Term-Definition do not have nested <a> or <span>
-            if (typeof term['Term-Definition'][0]['p'][0] === 'string') {
-              newTerm['Term-Definition'] = term['Term-Definition'][0]['p'][0];
-            // Check if Term-Definition has nested <a> or <span> as a nested object
-            } else if (typeof term['Term-Definition'][0]['p'][0] === 'object') {
-              let termString = '';
-              let termLink = '';
-              // Check if Term-Definition has a nested <span>
-              if (term['Term-Definition'][0]['p'][0]['span']) {
-                termString = term['Term-Definition'][0]['p'][0]['span'][0]['_'];
-              // Otherwise check if Term-Definition has a nested <a>
-              } else if (term['Term-Definition'][0]['p'][0]['a']) {
-                termString = term['Term-Definition'][0]['p'][0]['_'];
-                termLink = term['Term-Definition'][0]['p'][0]['a'][0]['_'];
+    // Check if Term collection has any documents
+    Term.count({}, (err, count) => {
+      console.log(count);
+      if (count === 0) {
+        // Parser for converting xml to json
+        const parser = new xml2js.Parser();
+        // Parse terms xml and convert to json
+        fs.readFile(path.resolve(__dirname, '../../resources/bedes_online_dictionary_uuid-terms.xml'), function(err, data) {
+          parser.parseString(data, function (err, result) {
+            let termsList = result.nodes.node;
+            // Save terms to DB
+            termsList.forEach((term, index) => {
+              let newTerm = new Term();
+              // Set the term's keys and values
+              newTerm['Content-UUID'] = term['Content-UUID'][0];
+              newTerm['URL'] = term['URL'][0];
+              newTerm['Term'] = term['Term'][0];
+              newTerm['Updated-date'] = term['Updated-date'][0];
+              newTerm['Category'] = term['Category'][0];
+              // Check if Term-Definition exist in current term
+              if (term['Term-Definition'][0]['p']) {
+                // This is the simplest case when Term-Definition does not have an <a> or <span> nested inside of it.
+                // Most of the Term-Definition do not have nested <a> or <span>
+                if (typeof term['Term-Definition'][0]['p'][0] === 'string') {
+                  newTerm['Term-Definition'] = term['Term-Definition'][0]['p'][0];
+                // Check if Term-Definition has nested <a> or <span> as a nested object
+                } else if (typeof term['Term-Definition'][0]['p'][0] === 'object') {
+                  let termString = '';
+                  let termLink = '';
+                  // Check if Term-Definition has a nested <span>
+                  if (term['Term-Definition'][0]['p'][0]['span']) {
+                    termString = term['Term-Definition'][0]['p'][0]['span'][0]['_'];
+                  // Otherwise check if Term-Definition has a nested <a>
+                  } else if (term['Term-Definition'][0]['p'][0]['a']) {
+                    termString = term['Term-Definition'][0]['p'][0]['_'];
+                    termLink = term['Term-Definition'][0]['p'][0]['a'][0]['_'];
+                  }
+                  // Set Term-Definition to the term's string text concatenated to the term's link
+                  newTerm['Term-Definition'] = termString + termLink;
+                }
+              } else {
+                // If there is no Term-Definition, set it to empty string
+                newTerm['Term-Definition'] = '';
               }
-              // Set Term-Definition to the term's string text concatenated to the term's link
-              newTerm['Term-Definition'] = termString + termLink;
-            }
-          } else {
-            // If there is no Term-Definition, set it to empty string
-            newTerm['Term-Definition'] = '';
-          }
-          newTerm['Application'] = term['Application'][0];
-          newTerm['Sector'] = term['Sector'][0];
-          newTerm['Unit-of-Measure'] = term['Unit-of-Measure'][0];
+              newTerm['Application'] = term['Application'][0];
+              newTerm['Sector'] = term['Sector'][0];
+              newTerm['Unit-of-Measure'] = term['Unit-of-Measure'][0];
 
-          // Save the term
-          newTerm.save(function(err) {
-            if (err) {
-              throw err;
-            }
-            // console.log('newTerm saved...');
-          });
-        });
-
-        console.log('Done');
-      });
-    });
-
-    // Parse options xml and convert to json
-    fs.readFile(path.resolve(__dirname, '../../resources/bedes_online_dictionary_uuid-lo.xml'), function(err, data) {
-      parser.parseString(data, function (err, result) {
-        // console.dir(result);
-        res.send(result.nodes.node[0]);
-        let OptionList = result.nodes.node;
-        // Save terms to DB
-        OptionList.forEach((option, index) => {
-          let newOption = {};
-          // Set the option's keys and values
-          newOption['Content-UUID'] = option['Content-UUID'][0];
-          newOption['URL'] = option['URL'][0];
-          newOption['Term'] = option['Term'][0];
-          newOption['Updated-date'] = option['Updated-date'][0];
-          newOption['Related-Term'] = option['Related-Term'][0];
-          newOption['Related-Term-UUID'] = option['Related-Term-UUID'][0];
-          newOption['Option-Definition'] = option['Option-Definition'][0];
-          newOption['Application'] = option['Application'][0];
-          newOption['Sector'] = option['Sector'][0];
-          newOption['Unit-of-Measure'] = option['Unit-of-Measure'][0];
-
-          // Find term associated with current option
-          Term.findOne({ 'Content-UUID': option['Related-Term-UUID'][0] }, function (err, term) {
-            if (err) {
-              return handleError(err);
-            }
-            // Save the updated term
-            if (term) {
-              term['Options'].push(newOption);
-              term.save(function(err) {
+              // Save the term
+              newTerm.save(function(err) {
                 if (err) {
                   throw err;
                 }
-                // console.log('Updated term saved...');
+                // console.log('newTerm saved...');
               });
-            }
-          })
-        });
-        console.log('Done');
-      });
-    });
+            });
 
+            console.log('Done');
+          });
+        });
+
+        // Parse options xml and convert to json
+        fs.readFile(path.resolve(__dirname, '../../resources/bedes_online_dictionary_uuid-lo.xml'), function(err, data) {
+          parser.parseString(data, function (err, result) {
+            // console.dir(result);
+            res.send(result.nodes.node[0]);
+            let OptionList = result.nodes.node;
+            // Save terms to DB
+            OptionList.forEach((option, index) => {
+              let newOption = {};
+              // Set the option's keys and values
+              newOption['Content-UUID'] = option['Content-UUID'][0];
+              newOption['URL'] = option['URL'][0];
+              newOption['Term'] = option['Term'][0];
+              newOption['Updated-date'] = option['Updated-date'][0];
+              newOption['Related-Term'] = option['Related-Term'][0];
+              newOption['Related-Term-UUID'] = option['Related-Term-UUID'][0];
+              newOption['Option-Definition'] = option['Option-Definition'][0];
+              newOption['Application'] = option['Application'][0];
+              newOption['Sector'] = option['Sector'][0];
+              newOption['Unit-of-Measure'] = option['Unit-of-Measure'][0];
+
+              // Find term associated with current option
+              Term.findOne({ 'Content-UUID': option['Related-Term-UUID'][0] }, function (err, term) {
+                if (err) {
+                  return handleError(err);
+                }
+                // Save the updated term
+                if (term) {
+                  term['Options'].push(newOption);
+                  term.save(function(err) {
+                    if (err) {
+                      throw err;
+                    }
+                    // console.log('Updated term saved...');
+                  });
+                }
+              })
+            });
+            console.log('Done');
+          });
+        });
+
+      } else {
+        // Term collectoin has documents so don't import xml into DB
+        console.log('Term collection already has documents inside...');
+      }
+    });
   });
 
   return app;
