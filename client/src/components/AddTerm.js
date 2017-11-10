877d7c22efc2441d28aa4ac61e742e535251588c
Xin Liu <xhliu97@gmail.com>
2017-11-10 11:30:39 -0500

    in progress: parser not working

--- client/src/components/AddTerm.js
+++ client/src/components/AddTerm.js
@@ -7,7 +7,7 @@ export default class AddTerm extends Component {
     this.state = {
       uuid: '',
       url: '',
-      term: '',
+      _term: '',
       date: '',
       category: '',
       definition: '',
@@ -15,27 +15,36 @@ export default class AddTerm extends Component {
       sector: '',
       measure: ''
     };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
+  handleChange(e, _state) {
+      this.setState({
+          [_state]: e.target.value
+      });
+  };
+
   handleSubmit(e) {
-      var _this = this;
-      axios.post('http:localhost:5000/api/term/add/', {
-          'Content-UUID': _this.uuid.value,
-          'URL': _this.url.value,
-          'Term': _this.term.value,
-          'Updated-date': _this.date.value,
-          'Category': _this.category.value,
-          'Term-Definition': _this.definition.value,
-          'Application': _this.application.value,
-          'Sector': _this.sector.value,
-          'Unit-of-Measure': _this.measure.value
+      e.preventDefault();
+      axios.post('http://localhost:5000/api/term/add/', {
+        'Content-UUID': this.state.uuid,
+        'URL': this.state.url,
+        'Term': this.state._term,
+        'Updated-date': this.state.date,
+        'Category': this.state.category,
+        'Term-Definition': this.state.definition,
+        'Application': this.state.application,
+        'Sector': this.state.sector,
+        'Unit-of-Measure': this.state.measure
       })
       .then(res =>
           console.log(res)
       )
-      .catch(err => {
-          console.log(err)
-      })
+      .catch(err =>
+          console.log(err))
+
+      this.props.history.push('/');
   }
 
   render() {
@@ -49,15 +58,15 @@ export default class AddTerm extends Component {
                 <div className="row">
                   <div className="col s12">
                         <form className="col s12" onSubmit={this.handleSubmit}>
-                          <input type="text" placeholder="Content-UUID" name="uuid" ref={uuid => this.uuid = uuid} />
-                          <input type="text" placeholder="URL" name="url" ref={url => this.url = url}/>
-                          <input type="text" placeholder="Term" name="term" ref={term => this.term = term}/>
-                          <input type="text" placeholder="Updated-date" name="date" ref={date => this.date = date}/>
-                          <input type="text" placeholder="Category" name="category" ref={category => this.category = category}/>
-                          <input type="text" placeholder="Term-Definition" name="definition" ref={definition => this.definition = definition}/>
-                          <input type="text" placeholder="Application" name="application" ref={application => this.application = application}/>
-                          <input type="text" placeholder="Sector" name="sector" ref={sector => this.sector = sector}/>
-                          <input type="text" placeholder="Unit-of-Measure" name="measure" ref={measure => this.measure = measure}/>
+                          <input type="text" placeholder="Content-UUID" name="uuid" value={this.state.uuid} onChange={(e) => this.handleChange(e, 'uuid') }/>
+                          <input type="text" placeholder="URL" name="url" value={this.state.url} onChange={(e) => this.handleChange(e, 'url') }/>
+                          <input type="text" placeholder="Term" name="_term" value={this.state._term} onChange={(e) => this.handleChange(e, '_term') }/>
+                          <input type="text" placeholder="Updated-date" name="date" value={this.state.date} onChange={(e) => this.handleChange(e, 'date') }/>
+                          <input type="text" placeholder="Category" name="category" value={this.state.category} onChange={(e) => this.handleChange(e, 'category') }/>
+                          <input type="text" placeholder="Term-Definition" name="definition" value={this.state.definition} onChange={(e) => this.handleChange(e, 'definition') }/>
+                          <input type="text" placeholder="Application" name="application" value={this.state.application} onChange={(e) => this.handleChange(e, 'application') }/>
+                          <input type="text" placeholder="Sector" name="sector" value={this.state.sector} onChange={(e) => this.handleChange(e, 'sector') }/>
+                          <input type="text" placeholder="Unit-of-Measure" name="measure" value={this.state.measure} onChange={(e) => this.handleChange(e, 'measure') }/>
                           <input type="submit" value="Submit" className="waves-effect waves-light btn"/>
                         </form>
                       </div>
