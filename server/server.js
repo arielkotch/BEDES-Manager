77b11c99137309b1136c0187807d7a47e3afac81
Ted Shin <tdyshin@gmail.com>
2017-11-11 22:27:42 -0500

    Add passport initialization and other packages to server.js

--- server/server.js
+++ server/server.js
@@ -1,12 +1,13 @@
 const dotenv = require('dotenv').config();
 const express = require('express');
-const exphbs = require('express-handlebars');
+const helmet = require('helmet'); // sets some http header for security
 const path = require('path');
+const flash = require('connect-flash');
+const cookieParser = require('cookie-parser');
 const session = require('express-session');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
-const mongoose = require('mongoose');
-// const MongoStore = require('connect-mongo')(session);
+const mongoose = require('mongoose'); // MongoDB object modeling tool designed to work in an asynchronous environment
 
 const configureServer = (app) => {
   // MongoDB Set Up (URI @ mongodb://localhost:27017/maalka_bedes)
@@ -18,18 +19,28 @@ const configureServer = (app) => {
     process.exit();
   });
 
-  // Logger
-  app.use(morgan('dev'));
+  // pass passport for configuration
+  require('./config/passport')(passport);
 
-
-  // Routes for CSS, JS etc.
-  // app.use(express.static(path.resolve(__dirname, '../frontend/controllers'), { redirect: false }));
+  app.use(morgan('dev')); // log every request to the console
+  app.use(helmet());
+  app.use(cookieParser()); // read cookies (needed for auth)
 
   // handle json data
   app.use(bodyParser.json());
   // handle URL-encoded data
   app.use(bodyParser.urlencoded({ extended: true }));
 
+  // required for passport
+  app.use(session({
+      secret: 'kjfdk1231lkdfsa0DGdkCa23k2',
+      resave: false,
+      saveUninitialized: false
+  }));
+
+  app.use(passport.initialize());
+  app.use(passport.session()); // persistent login sessions
+  app.use(flash()); // use connect-flash for flash messages stored in session
 }
 
 module.exports = configureServer;
