826cf1f6ed63f4a40f01bda2a1348b9c8b1d5dab
Xin Liu <xhliu97@gmail.com>
2017-11-25 14:44:11 -0500

    added client node modules into root package.json; to install all modules in one go

--- client/src/index.js
+++ client/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import 'semantic-ui-css/semantic.min.css';
 
-import './index.css';
+import './css/index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
