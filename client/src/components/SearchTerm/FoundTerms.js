66ae8966cf3452450514fe9a9fbd2d5b4dd1b198
Ted Shin <tdyshin@gmail.com>
2017-12-05 16:35:37 -0500

    Show options in searched terms

--- client/src/components/SearchTerm/FoundTerms.js
+++ client/src/components/SearchTerm/FoundTerms.js
@@ -8,12 +8,38 @@ const TransformsAccordian = (props) => {
   console.log('termData length: ' + dataLen);
 
   const arrayPanels = termData.map((obj, i) => {
-
     const objContents = [];
     for (var key in obj) {
       if (obj.hasOwnProperty(key)) {
-        console.log(key);
-        if (key != 'Options') {
+        // if key === Options, we need to create a nested accordian for the options
+        if (key === 'Options') {
+          const optionsArr = obj[key];
+          const optionsLen = optionsArr.length
+
+          const optionsArrPanels = optionsArr.map((optionObj, i) => {
+            const optionObjContents = [];
+            for (var key in optionObj) {
+              if (optionObj.hasOwnProperty(key)) {
+                optionObjContents.push(
+                  <p key={ key + '_' + i }>{ key }: { optionObj[key] }</p>
+                );
+              }
+            }
+
+            return { title: i + ': Object - ' + optionObj.Term, content: { content: optionObjContents }, key: 'option_panel_' + i };
+          });
+
+          const optionsArrContent = (
+            <Accordion.Accordion panels={ optionsArrPanels } />
+          );
+
+          const optionsRootPanel = [
+            { title: 'Options: Array [' + optionsLen + ']', content: { content: optionsArrContent }, key: 'option_root_panel' }
+          ];
+          objContents.push(
+            <Accordion panels={ optionsRootPanel }  styled />
+          );
+        } else {
           objContents.push(
             <p key={ key + '_' + i }>{ key }: { obj[key] }</p>
           );
@@ -21,17 +47,11 @@ const TransformsAccordian = (props) => {
       }
     }
 
-    return { title: i + ': Object', content: { content: objContents }, key: 'panel_' + i };
+    return { title: i + ': Object - ' + obj.Term, content: { content: objContents }, key: 'panel_' + i };
   });
 
-
-
-
-
   const arrayContent = (
-    <div>
-      <Accordion.Accordion panels={ arrayPanels } />
-    </div>
+    <Accordion.Accordion panels={ arrayPanels } />
   );
 
   const rootPanel = [
